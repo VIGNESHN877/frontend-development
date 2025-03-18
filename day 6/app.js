ce63c1801383ff18b2000e6f8917e08d9d700bca
VIGNESHN877 <vigneshn877@gmail.com>
2025-03-18 21:37:13 +0530

    this  a java script node js and express js file

---
+++ day 6/app.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+const requestHandler = (req,res)=>{
+
+    const url = req.url;
+    const method = req.method;
+    if (url ==='/' )
+        res.setHeaader('Content-Type', 'text/html');
+    res.write('<html')
+    res.write('<head><title>Enter From Details <title></head>');
+    res.write('<body><form action="/message" method="POST"><input type="text" name="input placeholder">');
+    res.write('</html>');
+
+
+    if (url === '/message' && method === 'POST'){
+}  
